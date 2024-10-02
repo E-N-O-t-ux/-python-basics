44052b893f9e277675b2adeae48d50ef65860b56
E-N-O-t-ux <serfa444@gmail.com>
2024-10-02 19:40:35 +0600

    Lesson 1

---
+++ урок 2.py
@@ -0,0 +1,1 @@
+'''1 ПРАКТИКА'''def numbers(a, b, c, d):    sum_num = a + b + c + d    sred_arif = sum_num / 4    return sum_num, sred_arifprint(numbers(23, 45, 12, 56))'''2 ПРАКТИКА'''def temp(a):    far = a * 1.8 + 32    kel = a + 273.15    return far, kelprint(temp(25))
